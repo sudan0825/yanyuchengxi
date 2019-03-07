ddddd0d46bf7b72cf388e3db1f299cb1de68d523
Dan Su <dan@Dans-MacBook-Pro.local>
2019-03-07 11:31:58 -0800

    add search/sort function to events and change aboutus/readme content

--- src/components/aboutus/aboutus.js
+++ src/components/aboutus/aboutus.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import './aboutus.css'
 
 const Aboutus = (props)=>{
-    return (<div><h1>About Us</h1></div>)
+    return (<div id="aboutus">
+        <h1>About Us</h1>
+    <p>This project is starting at March 5th 2019. I want to build a website that helps people 
+        looking for the one. If you have any sugestion about how a social website like this should be,
+        please email me: sudan0825@gmail.com. I would love to make your idea to the real world!   </p>
+    <p>Nothing will be better than a couple meets each other at my website some day! :)</p>
+    </div>)
 }
 export default Aboutus;
--- src/containers/activities/activities.js
+++ src/containers/activities/activities.js
@@ -4,7 +4,7 @@ import './activities.css';
 
 
 class Activities extends Component {
-   
+
     state = {
         th: ["Activity/Event name", "Time", "City", "Zip", "Participants"],
         td: [["Grocery Shopping", "05/06/2019", "Sunnyvale", "94086", 10],
@@ -13,76 +13,47 @@ class Activities extends Component {
         ["15 miles Hiking", "04/20/2019", "Sunnyvale", "94087", 20],
         ["Cooking class with Chef", "09/12/2019", "Cupertino", "94028", 30],
         ["Video Game together", "12/31/2019", "Fremont", "93048", 10]],
-        Filter:{
-            elementType:'input',
-            elemConfig:{
-                type:'search',
-                placeholder:"search activity by name or city"
+        Filter: {
+            elementType: 'input',
+            elemConfig: {
+                type: 'search',
+                placeholder: "search activity by name or city"
             }
-        }
+        },
+        ascending: true
     }
-    search(){
-        let filter, row, namedata, citydata,i,name,city;
-       
+    search() {
+        let filter, row, namedata, citydata, i, name, city;
+
         filter = document.getElementsByClassName("inputUI")[0].children[2].value.toLowerCase();
         row = document.getElementById("activitytable").getElementsByTagName("tr");
-        for(i = 1; i<row.length;i++){
+        for (i = 1; i < row.length; i++) {
             name = row[i].getElementsByTagName('td')[0];
             city = row[i].getElementsByTagName('td')[2];
             namedata = name.textContent || name.innerText;
             citydata = city.textContent || city.innerText;
-            console.log(namedata, citydata)
-            if(namedata.toLowerCase().indexOf(filter) !== -1 || 
-               citydata.toLowerCase().indexOf(filter) !== -1){
+            if (namedata.toLowerCase().indexOf(filter) !== -1 ||
+                citydata.toLowerCase().indexOf(filter) !== -1) {
                 row[i].style.display = "";
-            }else{
+            } else {
                 row[i].style.display = "none"
             }
         }
     }
     sorttable(e) {
-       
+
         let n = this.state.th.indexOf(e.target.innerText.trim());
-        let table,rows, switching = true,direction = "ascending",shouldswitch,i,
-            switchedcount=0,firstrow, secondrow;
-        table = document.getElementById("activitytable");
-        while(switching){
-            switching = false;
-            rows = table.rows;
-            for(i = 1; i < rows.length-1; i++){
-                shouldswitch = false;
-                firstrow = rows[i].getElementsByTagName('td')[n];
-                secondrow = rows[i+1].getElementsByTagName('td')[n];
 
-                if(direction == "ascending"){
-                    if(firstrow.innerHTML.split(' ').join('').toLowerCase() > 
-                        secondrow.innerHTML.split(' ').join('').toLowerCase()){
-                            shouldswitch = true;
-                            break;
-                        }
-                }else if(direction = "descending"){
-                    if(firstrow.innerHTML.split(' ').join('').toLowerCase() < 
-                        secondrow.innerHTML.split(' ').join('').toLowerCase()){
-                            shouldswitch = true;
-                            break;
-                        }
-                }
+        let tabledata = this.state.td.slice();
+        let sequence = this.state.ascending;
 
+        if (sequence) {
 
-            }
-            if(shouldswitch){
-                rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
-                switching = true;
-                switchedcount++;
-
-            }else{
-                if(switchedcount === 0 && direction === "ascending"){
-                    direction = "descending";
-                    switching = true
-                }
-            }
-  
+            tabledata.sort((a, b) => (a[n] + "").localeCompare(b[n] + ""))
+        } else {
+            tabledata.sort((a, b) => (b[n] + "").localeCompare(a[n] + ""))
         }
+        this.setState({ td: tabledata, ascending: !sequence })
     }
 
     render() {
@@ -96,10 +67,10 @@ class Activities extends Component {
             return <tr key={idx + tr.join('')}>{datas}</tr>
         })
         return (<div>
-            <Input  
-                    elementType={this.state.Filter.elementType} 
-                    elemConfig={this.state.Filter.elemConfig}
-                    changed={this.search}></Input>
+            <Input
+                elementType={this.state.Filter.elementType}
+                elemConfig={this.state.Filter.elemConfig}
+                changed={this.search}></Input>
             <table id="activitytable">
                 <caption>Activities & Events</caption>
                 <thead onClick={(e) => this.sorttable(e)}>
